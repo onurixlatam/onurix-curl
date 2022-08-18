6ba335803babf081c80bb548db3ac34fa747f2ab
JAlejandroZ <a.zambrano@aplicacionesdinamicas.com>
2022-08-18 15:14:36 -0500

    correction example code

--- SendCALL.sh
+++ SendCALL.sh
@@ -1,3 +1,3 @@
 # También puede usar wget
-curl -X POST "https://www.onurix.com/api/v1/call/send" -d key="582876138efc20d00c3bd32c046edff0cdd3369061d4c5123747d" -d client="2412" -d phone="AQUI_EL_NUMERO_DE_CELULAR" -d message="Prueba desde curl"  -d voice="Penelope" -d retries="1" -d country-code="CO" -d leave-voicemail="AQUI_CONFIRMACION_BUZON_DE_VOZ" -d audio-code="AQUI_ID_AUDIO" -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json'
+curl -X POST "https://www.onurix.com/api/v1/call/send" -d key="AQUI_SU_KEY" -d client="AQUI_SU_CLIENT" -d phone="AQUI_EL_NUMERO_DE_CELULAR" -d message="AQUI_EL_MENSAJE_A_ENVIAR"  -d voice="AQUI_TIPO_DE_VOZ" -d retries="AQUI_NUMERO_DE_INTENTOS" -d country-code="CO" -d leave-voicemail="AQUI_CONFIRMACION_BUZON_DE_VOZ" -d audio-code="AQUI_ID_AUDIO" -H 'Content-Type: application/x-www-form-urlencoded' -H 'Accept: application/json'
 
