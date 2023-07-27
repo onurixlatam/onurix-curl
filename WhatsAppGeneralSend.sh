3979c456d34a22d200ad6d56c5c1416c5904607a
luis pacheco <laps1308@gmail.com>
2023-07-27 13:59:57 -0500

    modify WhatsApp files

--- WhatsAppGeneralSend.sh
+++ WhatsAppGeneralSend.sh
@@ -1,2 +1,2 @@
 # También puede usar wget
-curl -X POST "https://www.onurix.com/api/v1/whatsapp/send?client=AQUI_SU_KEY&key=AQUI_SU_CLIENT&template=AQUI_EL_NOMBRE_DE_LA_PLANTILLA" -H 'Content-Type: application/json' -d 'AQUI_EL_JSON_CON_LOS_VALORES_PARA_LA_PLANTILLA'
+curl -X POST "https://www.onurix.com/api/v1/whatsapp/send" -d client=AQUI_SU_KEY -d key=AQUI_SU_CLIENT -d template=AQUI_EL_NOMBRE_DE_LA_PLANTILLA" -H 'Content-Type: application/json' -d 'AQUI_EL_JSON_CON_LOS_VALORES_PARA_LA_PLANTILLA'
