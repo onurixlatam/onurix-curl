a7dbfbc56b4a6df9b501bf6b9c801d303a2a9b58
dev2-apdi <dev2@aplicacionesdinamicas.com>
2025-08-28 18:34:19 -0500

    added new endpoint

---
+++ WhatsApp/SendWhatsAppWithoutTemplate.sh
@@ -0,0 +1,1 @@
+curl --location --globoff 'https://www.onurix.com/api/v1/whatsapp/send_without_template?key=AQUI_SU_SECRET_KEY&client=AQUI_SU_CLIENT_ID' --header 'Content-Type: application/json' --data '{"from_phone_meta_id":"AQUI_EL_META_ID_DEL_TELEFONO","phone":"AQUI_EL_TELEFONO_DESTINO","message":{"type":"text","value":"AQUI_EL_MENSAJE"}}'
